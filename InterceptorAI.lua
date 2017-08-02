d92ae9a1e28266cb0c3f820d6e82564cf05b5f7b
Frostbite <Frostbite@frosty-nee.net>
2017-08-02 18:03:31 -0400

    removed a bunch of bad code, added purge missiles func

--- InterceptorAI.lua
+++ InterceptorAI.lua
@@ -3,64 +3,49 @@ InterceptorWeaponSlot = 5
 IncomingMissiles = {}
 TargetedMissiles = {}
 function Update(I)
-
 	InterceptorLaunchers = GetLaunchers(I)
 	IncomingMissiles = GetWarnings(I)
-	ControlledMissiles = GetControlledMissiles(I)
+	
 
-	if table.getn(IncomingMissiles) > 0 then
-		for i=0, table.getn(IncomingMissiles)-1, 1 do
-			FireInterceptor(I)
-			ControlledMissiles = GetControlledMissiles(I)
-			I:SetLuaControlledMissileInterceptorTarget(0, table.getn(ControlledMissiles)-1 , 0, IncomingMissiles[i].Id)
-
-		end
-	end
-end
-
+	PurgeOldMissiles(I)
+end 
 
 function GetWarnings(I)
 --Returns an array of all missile warnings
 	warnings = {}
 	count = I:GetNumberOfWarnings(0)
 	if count > 0 then
-		for i=0 , count-1, 1 do
+		for i=0 , count, 1 do
 			warnings[i] = I:GetMissileWarning(0,i)
 		end
 	end
 	return warnings
 end
 
-function CheckWarningStatus(I)
---Checks each specific incoming missile and updates hostile missile array accordingly
-end
 
 
-function GetControlledMissiles(I)
-	count = I:GetLuaControlledMissileCount(0)
- 	missiles = {}
-	for i=0, count-1, 1 do
-		if I:IsLuaControlledMissileAnInterceptor(0,i) then
-			table.insert(missiles, i,I:GetLuaControlledMissileInfo) 
-			
+function PurgeOldMissiles(I)
+	for i=0, I:GetLuaTransceiverCount(),1 do
+		for n=0, I:GetLuaControlledMissileCount(i), 1 do
+			if I:GetLuaControlledMissileInfo(i,n).TimeSinceLaunch > 3.0 then
+				if I:IsLuaControlledMissileAnInterceptor(i,n) then
+					I:DetonateLuaControlledMissile(i,n)
+				end
+			end
 		end
 	end
-	return missiles
-			
 end
-
 
 
 function FireInterceptor(I)
 --Fires first available missile interceptor
-	Count = I:GetWeaponCount()
 	Weapons = {}
-	for i=0,Count-1 do
+	for i=0,I:GetWeaponCount()-1, 1 do
 		table.insert(Weapons, i, I:GetWeaponInfo(i))
 	end
-	for i=0,table.getn(Weapons)-1, 1 do
-		if Weapons[i].WeaponSlot == 5 then
-			if I:FireWeapon(i,5) == then
+	for i=0, table.getn(Weapons)-1, 1 do
+		if Weapons[i].WeaponSlot == InterceptorWeaponSlot then
+			if I:FireWeapon(i, InterceptorWeaponSlot) then
 				break
 			end
 		end
