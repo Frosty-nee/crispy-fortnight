27a3c9691a7028565036cbe42956f5374244f527
frosty-nee <frostbite@frosty-nee.net>
2018-04-20 01:04:09 -0400

    got hardcoded targeting working to start working on basic guidance

--- Guidance.lua
+++ Guidance.lua
@@ -6,8 +6,9 @@ function ActiveMissileUpdate(I)
 	ActiveMissiles[0] = nil
 	for i=0,I:GetLuaTransceiverCount(), 1 do
 		for o=0, I:GetLuaControlledMissileCount(i), 1 do
-			warning = I:GetLuaControlledMissileInfo(i,o)
-			if warning.Id ~= 0 then
+			local warning = I:GetLuaControlledMissileInfo(i,o)
+			-- don't add interceptors or null missiles to the list 
+			if warning.Id ~= 0 and I:IsLuaControlledMissileAnInterceptor(i,o) == false then
 				ActiveMissileTargets[warning.Id] = nil
 				ActiveMissiles[warning.Id] = warning
 			end
@@ -16,13 +17,40 @@ function ActiveMissileUpdate(I)
 end
 
 function AssignMissileTargets(I)
+--not sure how to do this well yet
+--might need config options to set how much to spread missiles among targets
+	for k,_ in pairs(ActiveMissiles) do
+		ActiveMissileTargets[k] = 15486		
+	end
+end
 
+function UpdateTargetList(I)
+	for i=0,I:GetNumberOfMainframes(),1 do
+		for o=0, I:GetNumberOfTargets(i),1 do
+			local tgt = I:GetTargetInfo(i,o)
+			if tgt.Id ~= 0 then
+				Targets[tgt.Id] = tgt
+			end
+		end
+	end
+end
+
+function RemoveOldMissileTargets(I)
+	for k,v in pairs(ActiveMissileTargets) do
+		if ActiveMissiles[k] == nil then
+			ActiveMissileTargets[k] = nil
+		end
+	end
 end
 
 function Update(I)
 	ActiveMissiles = {} -- clears old missiles that are no longer active
+	Targets = {}
+
 	ActiveMissileUpdate(I)
+	UpdateTargetList(I)
+	AssignMissileTargets(I)
 	for k,v in pairs(ActiveMissiles) do
-		I:LogToHud(k)
+		I:LogToHud("missileid: " .. tostring(k) .. "tgt: " .. ActiveMissileTargets[k])
 	end
 end
