3aa1a33b7acfc48db4788fcfe062bc53955d8e0f
frosty-nee <frostbite@frosty-nee.net>
2019-02-09 00:41:09 -0500

    Added function to control heading

--- ShipControl.lua
+++ ShipControl.lua
@@ -1,5 +1,6 @@
 --config
-
+--set this to the subconstruct ID of the spin block you place to control heading
+heading_spinner_id = 5
 
 --setup
 HydrofoilCount = 0
@@ -37,7 +38,7 @@ end
 
 function BalanceRoll(I, axes)
     -- roll to right counts down from 360, to left counts up from 0
-	vel_vector = math.sign(I:GetForwardsVelocityMagnitude())
+	vel_vector = Mathf.Sign(I:GetForwardsVelocityMagnitude())
     roll = I:GetConstructRoll()
     if roll > 180 then
         roll = -1.0 * math.abs(360.0-roll)
@@ -57,12 +58,38 @@ function BalanceRoll(I, axes)
     end
 end
 
+function MaintainHeading(I)
+	if I:Component_GetCount(1) > 0 then
+		-- gets a value from the first drive maintainer (-1 to 1) and maps it to a requested headin
+		requested_heading = (I:GetSubConstructInfo(heading_spinner_id).LocalRotation.w / 2 + 0.5) * 360
+		actual_heading = I:GetConstructYaw()
+	end
+	I:LogToHud(string.format('Requested: %.2f', requested_heading))
+	
+	magnitude = math.abs(requested_heading - actual_heading)
+	--positive is a right turn, negative is a left turn
+	direction = Mathf.Sign(requested_heading - actual_heading)
+	if magnitude > 180 then
+		magnitude = math.abs(magnitude - 360)
+		direction = direction * -1
+	end
+	--invert direction if moving backwards
+	if Mathf.Sign(I:GetForwardsVelocityMagnitude()) < 0 then
+		direction = direction * -1
+	end
+	if direction > 0 then
+		I:RequestControl(0, 1, magnitude/180)
+	else
+		I:RequestControl(0,0, magnitude/180)
+	end
+end
 
-function Update(I)
+	function Update(I)
     if HydrofoilCount ~= I:Component_GetCount(8) then
         HydrofoilAxes = AssignHydrofoilAxes(I)
         HydrofoilCount = I:Component_GetCount(8)
     end
 
-    AttitudeControl(I)
+	BalanceRoll(I, HydrofoilAxes)
+	MaintainHeading(I)
 end
